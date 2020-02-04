6af5bc5b769f09424bd52d06c44b17ba7a744db2
nadimkaysar <nadimkaysar1@gmail.com>
2020-02-04 16:45:25 +0600

    Git Auth Add V1

---
+++ AuthSystem/AuthSystem/Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AuthSystem.Models;
+using System.Web.Security;
+namespace AuthSystem.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Models.Membership model)
+        {
+             using (var context = new officeEntities())
+            {
+                bool isValid = context.User.Any(x => x.UserName == model.UserName && x.Password == model.Password);
+                if(isValid)
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, false);
+                    return RedirectToAction("Index", "Employees");
+                }
+              
+                ModelState.AddModelError("", "Invalid UserName and Password");
+                return View();
+            
+            }         
+        }
+        // GET: Account
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Signup(User model)
+        {
+            using (var context = new officeEntities())
+            {
+                context.User.Add(model);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Login");
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
